444096278425f77a9e333b3b46120cdf6c5f45e8
Darko Andreev <darko.andreev@paysafe.com>
2021-06-23 17:34:36 +0300

    added welcome screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,17 @@ import { NgModule } from '@angular/core';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: 'expenses',
     loadChildren: () => import('./expense/expense.module').then(m => m.ExpenseModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
   }
 ];
 
